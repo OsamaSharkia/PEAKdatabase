ce53dc9877d90d19be3e60e0b95c9ee214270d9d
Osama Sharkia <124062343+OsamaSharkia@users.noreply.github.com>
2025-04-19 15:42:22 +0100

    Delete JS directory

--- JS/supabase.js
+++
@@ -1,9 +0,0 @@
-// JS/supabase.js
-const SUPABASE_URL = 'https://znyoofmcjiknvevezbhb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpueW9vZm1jamlrbnZldmV6YmhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNjc1MjMsImV4cCI6MjA2MDY0MzUyM30.BG07ZfmdfEOhwJDcFkXVe3JHYB1GkcWceyn7nW9hGr0';
-
-// Initialize the Supabase client
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Export for use in other files
-window.supabaseClient = supabaseClient;
